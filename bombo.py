236d0c1fcf0045282b388ea84200ee5e3f1264ba
AngelMeji <joseangelmejiamedina@gmail.com>
2026-04-23 23:31:03 -0500

    feat: crear clase Bombo con gestión de números y mezcla aleatoria

--- bombo.py
+++ bombo.py
@@ -1,11 +1,20 @@
-"""
-Representa el bombo del juego de bingo.
+import random
 
-Responsabilidades:
-- Mantener los números disponibles.
-- Extraer números aleatorios sin repetir.
-- Registrar el historial de números extraídos.
 
-Relación:
-- Forma parte del Juego (composición).
-"""
+class Bombo:
+    """
+    Representa el bombo del juego de bingo.
+
+    Responsabilidades:
+    - Mantener los números disponibles.
+    - Extraer números aleatorios sin repetir.
+    - Registrar el historial de números extraídos.
+
+    Relación:
+    - Forma parte del Juego (composición).
+    """
+
+    def __init__(self, max_num: int):
+        self.numeros = list(range(1, max_num + 1))
+        random.shuffle(self.numeros)
+        self.historial: list[int] = []
